fdfd212a116162fc5ffbe68aaa08f7305799d12b
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-02-17 14:51:00 +0100

    Window resize handler.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import addWindowResizeCallback from '../util/windowresize';
 
 interface Point {
   x: number;
@@ -6,48 +7,56 @@ interface Point {
 }
 
 const drawPoints = (ctx: CanvasRenderingContext2D, points: Point[]) => {
-  ctx.fillStyle = '#333333';
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   ctx.fillStyle = '#666666';
   points.forEach((point) => {
     ctx.beginPath();
-    ctx.arc(point.x, point.y, 10, 0, Math.PI * 2);
+    ctx.arc(point.x, point.y, 16, 0, Math.PI * 2);
     ctx.fill();
   });
+};
+
+const drawCanvas = (canvas: HTMLCanvasElement | null, points: Point[]) => {
+  if (canvas) {
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      ctx.fillStyle = '#333333';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      drawPoints(ctx, points);
+    }
+  }
 };
 
 export default () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [points, setPoints] = useState<Point[]>([]);
-  const [dimensions, setDimensions] = useState({ 
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
+  const [rect, setRect] = useState<DOMRect>();
+
+  const onCanvasMousedown = useCallback((e: MouseEvent) => {
+    if (rect) {
+      setPoints([ ...points, { x: e.clientX - rect.left, y: e.clientY - rect.top }]);
+      drawCanvas(canvasRef.current, points);
+    }
+  }, [rect]);
   
-  useEffect(() => drawCanvas(), [points]);
+  useEffect(() => drawCanvas(canvasRef.current, points), [points]);
 
   useEffect(() => {
-    function handleResize() {
-      setDimensions({ height: window.innerHeight, width: window.innerWidth });
-      
-    }
-    window.addEventListener('resize', handleResize);
-  })
-
-  const drawCanvas = () => {
-    if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d');
-      if (ctx) {
-        drawPoints(ctx, points);
+    const onWindowResize = () => {
+      const parentEl = canvasRef.current?.parentElement;
+      if (parentEl) {
+        const parentRect = parentEl.getBoundingClientRect();
+        canvasRef.current.width = parentRect.width;
+        canvasRef.current.height = parentRect.height;
+        setRect(parentRect);
+        drawCanvas(canvasRef.current, points);
       }
-    }
-  };
+    };
+    addWindowResizeCallback(onWindowResize);
+    onWindowResize();
+  }, []);
 
   return <canvas
-    ref={canvasRef}
-    onClick={(e) => {
-      setPoints([ ...points, { x: e.clientX, y: e.clientY }]);
-      drawCanvas();
-    }}
+    ref = {canvasRef}
+    onMouseDown = {(e) => onCanvasMousedown(e.nativeEvent)}
     />;
 }
