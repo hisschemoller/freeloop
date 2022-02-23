904827c6ecc01af3b9eab5543af7a348c5a73341
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-02-23 14:37:11 +0100

    Bottom toolbar CSS for iOS Safari.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,11 +5,13 @@ function App() {
   return (
     <div className="bg-zinc-900 h-screen text-zinc-200">
       <div className="container mx-auto h-screen flex flex-col">
-        <div>
-          <button type="button" className="button">Click</button>
-        </div>
         <div className="bg-red-900 flex-grow">
           <Canvas />
+        </div>
+        <div className="toolbar">
+          <div className="pb-4 pt-1">
+            <button type="button" className="button">&gt;</button>
+          </div>
         </div>
       </div>
     </div>
